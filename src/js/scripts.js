b2f8a2abe4f37bfba89f2e1f91ecaf9da1348e9c
Stephanie Buffotot <stephaniebtot3@gmail.com>
2025-11-09 12:59:16 -0700

    Assignment 2 Upload

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,59 @@
-// eslint-disable-next-line no-unused-vars
-/* global output, input */
-// eslint-disable-next-line no-unused-vars
+
+
 async function main() {
-  // This is where the code you're actually experimenting with goes.
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+    // User input
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+    let total = parseFloat(prompt("Enter total cost of goods:"));
+    let cash = parseFloat(prompt("Enter amount received by customer:"));
+    let discountCode = parseInt(prompt("Enter a discount code between 0 - 5 :"));
+
+    // Calculate discount
+
+    let discountRate;
+
+    switch (discountCode) {
+        case 0:
+            discountRate = 0;
+            break;
+        case 1:
+            discountRate = 0.10;
+            break;
+        case 2:
+            discountRate = 0.15;
+            break;
+        case 3:
+            discountRate = 0.25;
+            break;
+        case 4:
+            discountRate = 0.35;
+            break;
+        case 5:
+            discountRate = 0.40;
+            break;
+        default:
+            alert("Invalid discount code! Please enter a number between 0 and 5.");
+            return;
+    }
+
+// Discount Calculation 
+
+    let discountAmount = total * discountRate;
+    let totalAfterDiscount = total - discountAmount;
+
+// Calculate change
+
+    let change = cash - totalAfterDiscount;
+
+  // Output
+
+
+
+if (change < 0) {
+  alert("Insufficient Payment!");
+} else {
+  alert("Change back to customer: $" + change.toFixed(2));
+
+}
+main();
 }
